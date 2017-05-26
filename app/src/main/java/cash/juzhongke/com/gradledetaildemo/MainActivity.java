7d43d60039790f6cd97d065a41cf994fd5738979
yxhuang <hyx@gdysj.com.cn>
2017-05-26 17:27:11 +0800

    first commit

---
+++ app/src/main/java/cash/juzhongke/com/gradledetaildemo/MainActivity.java
@@ -0,0 +1,18 @@
+package cash.juzhongke.com.gradledetaildemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Log  BuildConfig LOG CALL " + BuildConfig.LOG_CALLS );
+    }
+}
